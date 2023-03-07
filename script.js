7947f9af4b27d4a2c7fc32cd26f0b0235ace6315
shubham kumar <65395298+Shubham800210@users.noreply.github.com>
2023-03-07 21:42:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 // Your JS code here.
 function addSlide() {
-            var scrolledImages = document.querySelectorAll('.slide-in');
-            console.log("ScrolledImages",scrolledImages);
-            scrolledImages.forEach((img) => {
-                var scrolledHeight = window.scrollY + window.innerHeight - img.height / 2;
-                var imageBottomHeight = img.offsetTop + img.height;
-                var isImageHalfScrolled = scrolledHeight > img.offsetTop;
+  scrolledImages.forEach((img) => {
+    var scrolledHeight = window.scrollY + window.innerHeight - img.height / 2;
+    var imageBottomHeight = img.offsetTop + img.height;
+    var isImageHalfScrolled = scrolledHeight > img.offsetTop;
 
-                var isImageFullyScrolled = window.scrollY > imageBottomHeight;
-               
-                console.log("Height", scrolledHeight,imageBottomHeight, img.offsetTop, window.innerHeight);
-                if (isImageHalfScrolled && !isImageFullyScrolled) {
-                    img.classList.add("active");
-                } else {
-                    img.classList.remove("active");
-                }
-            });
-        }
-        window.addEventListener('scroll', debounce(addSlide));
+    var isImageFullyScrolled = window.scrollY > imageBottomHeight;
+
+    if (isImageHalfScrolled && !isImageFullyScrolled) {
+      img.classList.add("active");
+    } else {
+      img.classList.remove("active");
+    }
+  });
+}
